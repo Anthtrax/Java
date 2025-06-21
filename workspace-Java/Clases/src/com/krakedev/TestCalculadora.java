2c1d07d00d95091eb190117c0a205cbfe7c5611d
Anthtrax <antez.ama2020@gmail.com>
2025-06-20 23:05:32 -0500

    Funcion Multiplicar echo

---
+++ workspace-Java/Clases/src/com/krakedev/TestCalculadora.java
@@ -0,0 +1,24 @@
+package com.krakedev;
+
+public class TestCalculadora {
+
+	public static void main(String[] args) {
+		Calculadora calcu;
+		int resultadoSuma;
+		double resultadoResta;
+		double resultadoMultiplicacion;
+		
+		calcu=new Calculadora();
+		
+		resultadoSuma=calcu.sumar(5,3);
+		resultadoResta=calcu.restar(10,3);
+		resultadoMultiplicacion=calcu.multiplicar(10,50);
+		
+		
+		System.out.println("RESULTADO SUMA: "+resultadoSuma);
+		System.out.println("RESULTADO RESTA: "+resultadoResta);
+		System.out.println("RESULTADO MULTIPLICACION: "+resultadoMultiplicacion);
+
+	}
+
+}
