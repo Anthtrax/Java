50ac66d65b85ee0e1c8c46854f472b550f08a735
Anthtrax <antez.ama2020@gmail.com>
2025-06-20 23:10:22 -0500

    Funcion dividir echo

--- workspace-Java/Clases/src/com/krakedev/TestCalculadora.java
+++ workspace-Java/Clases/src/com/krakedev/TestCalculadora.java
@@ -7,17 +7,20 @@ public class TestCalculadora {
 		int resultadoSuma;
 		double resultadoResta;
 		double resultadoMultiplicacion;
+		double resultadoDividir;
 		
 		calcu=new Calculadora();
 		
 		resultadoSuma=calcu.sumar(5,3);
 		resultadoResta=calcu.restar(10,3);
 		resultadoMultiplicacion=calcu.multiplicar(10,50);
+		resultadoDividir=calcu.dividir(30,3);
 		
 		
 		System.out.println("RESULTADO SUMA: "+resultadoSuma);
 		System.out.println("RESULTADO RESTA: "+resultadoResta);
 		System.out.println("RESULTADO MULTIPLICACION: "+resultadoMultiplicacion);
+		System.out.println("RESULTADO DIVISION: "+resultadoDividir );
 
 	}
 
