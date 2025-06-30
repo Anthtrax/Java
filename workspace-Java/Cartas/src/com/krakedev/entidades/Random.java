228ce0970559200dff962e0543b5fc233bfe58a6
Anthtrax <antez.ama2020@gmail.com>
2025-06-30 17:50:41 -0500

    Reto 21 echo

---
+++ workspace-Java/Cartas/src/com/krakedev/entidades/Random.java
@@ -0,0 +1,9 @@
+package com.krakedev.entidades;
+
+public class Random {
+	public static double obtenerPosicion() {
+		double  numero =0;
+		numero=(Math.random()*51);
+		return numero;
+	}
+}
---
+++ workspace-Java/Cartas/src/com/krakedev/servicios/Jugadores.java
@@ -0,0 +1,44 @@
+package com.krakedev.servicios;
+
+import java.util.ArrayList;
+
+import com.krakedev.entidades.Carta;
+import com.krakedev.entidades.Numero;
+
+public class Jugadores {
+	private ArrayList<String> nombreJugadores = new ArrayList<String>();
+	private ArrayList<Integer> total = new ArrayList<Integer>();
+
+	public ArrayList<String> getJugadores() {
+		return nombreJugadores;
+	}
+
+	public void setJugadores(ArrayList<String> nombreJugadores) {
+		this.nombreJugadores = nombreJugadores;
+	}
+
+	public void jugar() {
+		ArrayList<String> jugadores = new ArrayList<String>();
+		jugadores.add("jugador 1");
+		jugadores.add("jugador 2");
+		jugadores.add("jugador 3");
+		Juego juegoO = new Juego(jugadores);
+		juegoO.entregarCartas(5);
+
+		ArrayList<ArrayList<Carta>> cartasJ = juegoO.getCartasJugador();
+		int totalpuntaje = 0;
+		for (int i = 0; i < cartasJ.size(); i++) {
+			ArrayList<Carta> cartas = cartasJ.get(i);
+			for (int j = 0; j < cartas.size(); j++) {
+				Carta ce = cartas.get(j);
+				Numero ne = ce.getNumero();
+				totalpuntaje = totalpuntaje + ne.getValor();
+				total.add(totalpuntaje);
+				System.out.println("Total del jugador: " + totalpuntaje);
+
+			}
+
+		}
+
+	}
+}
