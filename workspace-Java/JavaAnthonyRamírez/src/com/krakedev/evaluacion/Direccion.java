b1ab7f11929686d52d11947ccbe123a05594956f
Anthtrax <antez.ama2020@gmail.com>
2025-06-30 17:01:57 -0500

    Reto 20 echo

---
+++ workspace-Java/JavaAnthonyRamírez/src/com/krakedev/evaluacion/Direccion.java
@@ -0,0 +1,27 @@
+package com.krakedev.evaluacion;
+
+public class Direccion {
+	private String callePrincipla;
+	private String calleSecundaria;
+
+	public Direccion(String callePrincipla, String calleSecundaria) {
+		this.callePrincipla = callePrincipla;
+		this.calleSecundaria = calleSecundaria;
+	}
+
+	public String getCallePrincipla() {
+		return callePrincipla;
+	}
+
+	public void setCallePrincipla(String callePrincipla) {
+		this.callePrincipla = callePrincipla;
+	}
+
+	public String getCalleSecundaria() {
+		return calleSecundaria;
+	}
+
+	public void setCalleSecundaria(String calleSecundaria) {
+		this.calleSecundaria = calleSecundaria;
+	}
+}
---
+++ workspace-Java/JavaAnthonyRamírez/src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module JavaAnthonyRamírez {
+}
